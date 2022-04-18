b41162227c19a6e40b48fc2fa4a99539a32e1c96
Miko Suria <jose.suria@venn.bio>
2022-04-18 16:05:44 +0800

    Refactor, added storybook

--- components/form/Button/IvButton.vue
+++ components/form/Button/IvButton.vue
@@ -1,14 +1,12 @@
 <template>
     <button 
-            class="border border-blue-200
-                 bg-blue-100 
-                   px-5 
-                   py-2 
-                   text-sm
+            class="border border-blue-200  
+                   px-5 py-2 text-sm m-2
                  text-blue-700 
-                   rounded 
-                   m-2 "
+                 bg-blue-100
+                   rounded"
             :class="btnIsDisabled ? 'cursor-not-allowed bg-gray-200 text-gray-300': 'hover:bg-blue-500 hover:text-white'"
+            
             :disabled="btnIsDisabled ? 'disabled' : false"
             >
         {{ btnLabel }}
